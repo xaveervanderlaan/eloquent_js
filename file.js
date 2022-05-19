3bdd89accd44684b5426c929ee8fb34e0fa6e18d
Xaveer van der Laan <xavlaan@protonmail.com>
2022-05-19 21:39:02 +0200

    Name + create root folder, nest files in labeled folders!

--- file.js
+++ file.js
@@ -8,7 +8,7 @@ const rl = readline.createInterface({
 
 const content = 'Hello!'
 
-const writeTestFile = (fileName, directory) => {
+const createFile = (fileName, directory) => {
   try {
     fs.writeFileSync(`${directory}${fileName}.txt`, content)
     console.log(`wrote file: ${fileName}`)
@@ -17,19 +17,61 @@ const writeTestFile = (fileName, directory) => {
   }
 }
 
-const generateFiles = () => {
-  if (!fs.existsSync('./test')) {
-    fs.mkdirSync('./test')
-  }
-  rl.question('How many files do you want? \n', (amount) => {
-    if (amount === '0') {
-      console.error('ERROR - 0 is not allowed')
-    } else {
-      for (let x = 0; x < amount; x++) {
-        writeTestFile(x, './test/')
-      }
+const createFolder = (folderName) => {
+  try {
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName)
+      console.log(`created folder: ${folderName}`)
     }
-    rl.close()
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const convertYnAnswer = (ynAnswer) => {
+  const answers = {
+    yes: ['yes', 'y'],
+    no: ['no', 'n']
+  }
+
+  if (answers.yes.includes(ynAnswer)) {
+    return true
+  }
+
+  if (answers.no.includes(ynAnswer)) {
+    return false
+  }
+}
+
+// Ask a bunch of questions to generate folders and files alike!
+const generateFiles = () => {
+  rl.question('Folder name?\n', (folderName) => {
+    fs.mkdirSync(`./${folderName}`)
+    rl.question('How many files do you want? \n', (amount) => {
+      rl.question('Do you want a folder for every file?\n', (ynAnswer) => {
+        let x
+
+        const niceYnAnswer = convertYnAnswer(ynAnswer)
+
+        const folderPath = `./${folderName}/`
+
+        if (amount === '0') {
+          console.error('ERROR - 0 is not allowed')
+        } else {
+          for (x = 0; x < amount; x++) {
+            if (niceYnAnswer) {
+              const nestedPath = `${folderPath}${x}/`
+
+              createFolder(nestedPath)
+              createFile(x, nestedPath)
+            } else {
+              createFile(x, folderPath)
+            }
+          }
+        }
+        rl.close()
+      })
+    })
   })
 }
 
